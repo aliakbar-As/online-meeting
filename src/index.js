e3dc8cc1f8a097404f24bc66332b5be512f1b91f
Aliakbar-As <aliakbarasghari110@gmail.com>
2021-12-19 16:58:05 +0330

    turned to pwa

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import * as serviceWorker from './serviceworker';
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
@@ -16,4 +18,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorker.register();
 reportWebVitals();
